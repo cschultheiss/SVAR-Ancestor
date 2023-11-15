1e91b2095ed62681e676ebd227f13e4eeacc442a
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2023-11-15 14:44:56 +0100

    Get LiNGAM information

--- la-example.R
+++ la-example.R
@@ -1,10 +1,11 @@
 source("lin-anc-ts.R")
 nrep <- 1e3
-n <- 1e3
+n <- 1e2
 p <- 2
 deg <- 1
 ptot <- p * (deg + 1)
 zv <- pv <- array(NA, c(nrep, p, ptot))
+lg <- array(NA, c(nrep, p, p))
 for (r in 1:nrep){
   eps1 <- runif(n, -1, 1)
   eps2 <- runif(n, -1, 1)
@@ -19,6 +20,9 @@ for (r in 1:nrep){
   lat <- lin.anc.ts(cbind(x1, x2), deg)
   pv[r,,] <- lat$p.val
   zv[r,,] <- lat$z.val
+  X_can <- tsdata2canonicalform(cbind(x1, x2),deg) # put data into canonical form
+  result <- VARLiNGAM(X_can, est_meth="ols", ntests=FALSE, pruning=TRUE)
+  lg[r,,] <- result$Bhat[[1]]
 }
 dimnames(pv) <- dimnames(zv) <- list(1:nrep, rownames(lat$p.val), colnames(lat$p.val))
 
@@ -28,3 +32,4 @@ for(j in 1:p){
     plot.ecdf(pv[,j,k], main = paste(dimnames(pv)[[3]][k], "on", dimnames(pv)[[2]][j]))
   }
 }
+apply(lg != 0, 2:3, mean)
