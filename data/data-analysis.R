7d53debddcda7e712616d720c9fb3aa860768ca8
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2024-01-23 15:36:13 +0100

    Add data folder

---
+++ data/data-analysis.R
@@ -0,0 +1,23 @@
+require(MASS)
+source("~/Documents/ETH/PhD/SVAR-Ancestor/lin-anc-ts.R")
+
+# shift waiting such that it is waiting after erruption
+geyser2 <- data.frame(waiting = geyser$waiting[-1], duration = geyser$duration[-nrow(geyser)])
+
+la_geyser <- lin.anc.ts(geyser, 6)
+la_geyser2 <- lin.anc.ts(geyser2, 6)
+
+summary.graph(la_geyser)
+summary.graph(la_geyser2)
+# same summary graph
+
+instant.graph(la_geyser)
+instant.graph(la_geyser2)
+# only latter shows instant effect
+
+
+milk <- data.frame(butter = read.table("data/data_butter")$V1, cheddar = read.table("data/data_cheddar")$V1)
+
+la_milk <- lin.anc.ts(milk, 6)
+instant.graph(la_milk)
+summary.graph(la_milk)
