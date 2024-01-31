6d6d08882aa9fd82677b852453e4c853419e049f
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2024-01-31 17:45:20 +0100

    Add holm for vector

--- lin-anc-ts.R
+++ lin-anc-ts.R
@@ -142,3 +142,11 @@ holm.corr <- function(pv, cut = TRUE){
   }
   pv
 }
+
+holm.uncut <- function(pv){
+  p <- length(pv)
+  i <- seq_len(p)
+  o <- order(pv)
+  ro <- order(o)
+  cummax((p + 1L - i) * pv[o])[ro]
+}
