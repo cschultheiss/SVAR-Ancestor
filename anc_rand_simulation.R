310cd94c1f1cfd38b85083f9e3868027df627ef2
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2024-04-22 15:50:20 +0200

    Comments to simulation

--- anc_rand_simulation.R
+++ anc_rand_simulation.R
@@ -41,40 +41,42 @@ rand_simulation <- function(nsim = 1000, n.vec = 10^(2:6), n.init = 10^4){
   
   opts <- list(progress = progress)
   
-  p <- 6
-  nlag <- 1
+  p <- 6 # number of variables
+  nlag <- 1 # maximum lag considered
   
   RNGkind("L'Ecuyer-CMRG")
-  set.seed(42)
+  set.seed(42) # make it reproducible
   seed.vec <- sample(1:10000, length(n.vec))
   print(seed.vec) # 3588 3052 2252 5257 8307
   
-  As <- B1s <- array(0, c(p, p, nsim))
-  pers <- matrix(NA, p, nsim)
+  As <- B1s <- array(0, c(p, p, nsim)) # to store effect matrices
+  pers <- matrix(NA, p, nsim) # to store permutations of the error distributions
   for (s in 1:nsim){
-    rd <- randomDAG(p, 0.2, lB = 0.5, uB = 1)
+    rd <- randomDAG(p, 0.2, lB = 0.5, uB = 1) # create a random DAG
     
-    B <- matrix(0, p , p)
+    B <- matrix(0, p , p) # represent DAG as matrix
     for (i in 2:p){
       for(j in 1:(i-1)){
-        B[i,j] <- max(0, rd@edgeData@data[[paste(j,"|",i, sep="")]]$weight)
+        B[i,j] <- max(0, rd@edgeData@data[[paste(j,"|",i, sep="")]]$weight) # store edge weights
       }
     }
     
-    pers[, s] <- sample.int(p)
+    pers[, s] <- sample.int(p) # random permutation
     
-    A <- solve(diag(p) - B)
+    A <- solve(diag(p) - B) # total effects
     for (j in 2:p){
       varj <- sum(A[j,]^2) - 1
       if(varj != 0) {
-        B[j,] <- B[j,] / sqrt(varj) * runif(1, sqrt(1/2), sqrt(2))
-        A <- solve(diag(p) - B)
+        B[j,] <- B[j,] / sqrt(varj) * runif(1, sqrt(1/2), sqrt(2)) # standardize
+        A <- solve(diag(p) - B) # total effects
       }
     }
-    As[, , s] <- A
+    As[, , s] <- A # store to array
     
+    # random matrix for lagged effects
     B1 <- matrix(runif(p^2, 0.2, 0.8) * rbinom(p^2, 1, 0.1) * sample(c(-1, 1), p^2, TRUE), nrow = p)
-    Btild <- A %*% B1
+    Btild <- A %*% B1 # VAR matrix
+    # shrink if (almost) not stable
     eig.max <- max(abs(eigen(Btild)$values))
     if(eig.max > 0.95){
       B1 <- B1 / eig.max * 0.95
@@ -82,6 +84,7 @@ rand_simulation <- function(nsim = 1000, n.vec = 10^(2:6), n.init = 10^4){
     B1s[,, s] <- B1
   }
   setup <- list(As = As, B1s = B1s, pers = pers)
+  
   resname <- paste0("setup ", format(Sys.time(), "%d-%b-%Y %H.%M"))
   if (save) save(setup, file = paste("results/", newdir, "/", resname, ".RData", sep = ""))
   seed.n <- 0
@@ -89,7 +92,7 @@ rand_simulation <- function(nsim = 1000, n.vec = 10^(2:6), n.init = 10^4){
   
   for (n in n.vec) {
     print(n)
-    n <- n + n.init
+    n <- n + n.init # total sample size including burnin
     seed.n <- seed.n + 1
     set.seed(seed.vec[seed.n])
     
@@ -102,23 +105,22 @@ rand_simulation <- function(nsim = 1000, n.vec = 10^(2:6), n.init = 10^4){
                    
                    psi <- cbind(rt(n, 7) / sqrt(1.4), runif(n, -sqrt(3), sqrt(3)), rt(n, 7) / sqrt(1.4),
                                 rexp(n) * (2 * rbinom(n, 1, 0.5) - 1) / sqrt(2), rnorm(n),
-                                runif(n, -sqrt(3), sqrt(3)))
+                                runif(n, -sqrt(3), sqrt(3))) # get noise
+                   
                    # permute distribution
                    psi <- psi[, pers[, gu]]
                    
                    x <- psi
-                   laa <- list()
+                   # generate data from SVAR
                    x[1,] <- As[, , gu] %*% psi[1,]
                    for(i in 2:n){
                      x[i,] <- As[, , gu] %*% (B1s[, , gu] %*% x[i - 1, ] + psi[i, ])
                    }
                    colnames(x) <- paste("x", 1:p, sep = "")
-                   x <- x[-(1:n.init),]
+                   x <- x[-(1:n.init),] # discard burn-in
                    
-                   laa <- lin.anc.ts(x, degree = 1)
-                   outmat <- laa[[1]]
-                   
-                   
+                   laa <- lin.anc.ts(x, degree = 1) # apply ancestor regression
+                   outmat <- laa[[1]] # store test statistics
                    
                    out <- list()
                    out$res <- outmat
@@ -127,28 +129,30 @@ rand_simulation <- function(nsim = 1000, n.vec = 10^(2:6), n.init = 10^4){
                  } 
     toc()
     stopCluster(cl)
+    # store output list to matrix
     res.mat <- array(unlist(res[,"res"]), dim = c(p, (nlag + 1) * p, nsim), dimnames = list(rownames(res[1,"res"][[1]]),
                                                                                             colnames(res[1,"res"][[1]]), NULL))
     ind <- unlist(res[,"gu"])
     names(ind) <- NULL
     
     
-    n <- n - n.init
+    n <- n - n.init # adjust n
+    # store results
     simulation <- list(res = res.mat, n = n, ind = ind,
                        r.seed = attr(res, "rng"), "commit" = commit)
+    # create unique filename based on sample size and time
     resname <- paste0("results n=", n, " ", format(Sys.time(), "%d-%b-%Y %H.%M"))
+    # save the file to the folder
     if (save) save(simulation, file = paste("results/", newdir, "/", resname, ".RData", sep = ""))
     
     As1 <- As2 <- array(NA, dim(As))
     As1[abs(As) > 1e-5] <- 1
     As2[abs(As) < 1e-5] <- 1
-    for (r in 0:0) {
-      print(apply(res.mat[,(1:p) + (r * p), ] * As1[, , ind], 1:2, mean, na.rm = TRUE))
-      print(apply(res.mat[,(1:p) + (r * p), ] * As2[, , ind], 1:2, mean, na.rm = TRUE))
-    }
+    # print average z-statistics for instantaneous effects for ancestors and non-ancestors
+    print(apply(res.mat[,(1:p), ] * As1[, , ind], 1:2, mean, na.rm = TRUE))
+    print(apply(res.mat[,(1:p), ] * As2[, , ind], 1:2, mean, na.rm = TRUE))
   }
   return(paste("results/", newdir, sep = ""))
 }
-rand_simulation()
 
 
