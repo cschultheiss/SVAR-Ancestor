17b929994cdf5fd3ebce9888688389ee8e9ac677
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2024-04-22 16:24:14 +0200

    Reorder data analysis. Add p-values

--- data/data-analysis.R
+++ data/data-analysis.R
@@ -1,29 +1,39 @@
-require(MASS)
 source("lin-anc-ts.R")
+
+require(MASS) # for geyser data
+# analyze geyser data
+la_geyser <- lin.anc.ts(geyser, 6)
+instant.p.val(la_geyser)
+instant.graph(la_geyser)
+summary.p.val(la_geyser)
+summary.graph(la_geyser)
 
 # shift waiting such that it is waiting after erruption
 geyser2 <- data.frame(waiting = geyser$waiting[-1], duration = geyser$duration[-nrow(geyser)])
-
-la_geyser <- lin.anc.ts(geyser, 6)
 la_geyser2 <- lin.anc.ts(geyser2, 6)
-
-summary.graph(la_geyser)
-summary.graph(la_geyser2)
-# same summary graph
-
-instant.graph(la_geyser)
+instant.p.val(la_geyser2)
 instant.graph(la_geyser2)
-# only latter shows instant effect
+summary.p.val(la_geyser2)
+summary.graph(la_geyser2)
 
 
-milk <- data.frame(butter = read.table("data/data_butter.txt")$V1, cheddar = read.table("data/data_cheddar.txt")$V1)
-
-la_milk <- lin.anc.ts(milk, 6)
-instant.graph(la_milk)
-summary.graph(la_milk)
-
+# analyze gas furnace data
+# data should be stored in data/gas-furnace.csv available from https://openmv.net/info/gas-furnace
 gas_furnace <- read.csv("data/gas-furnace.csv")
 
 la_gas <- lin.anc.ts(gas_furnace, 6)
+instant.p.val(la_gas)
 instant.graph(la_gas)
+summary.p.val(la_gas)
 summary.graph(la_gas)
+
+# analyze dairy data
+# data should be stored in data/data_butter.txt and data/data_cheddar.txt
+# create data frame
+milk <- data.frame(butter = read.table("data/data_butter.txt")$V1, cheddar = read.table("data/data_cheddar.txt")$V1)
+
+la_milk <- lin.anc.ts(milk, 6)
+instant.p.val(la_milk)
+instant.graph(la_milk)
+summary.p.val(la_milk)
+summary.graph(la_milk)
